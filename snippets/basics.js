941db315e3ad7228248d96d9f4d8ad7202dbcf3d
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-01-11 15:08:20 +0530

    Loose Equality Operators

--- snippets/basics.js
+++ snippets/basics.js
@@ -8,4 +8,28 @@ const assets = [
 ]
 
 const videos = assets.filter(asset => asset.type === "video");
-console.log(videos)
+// console.log(videos)
+
+// 2. Loose Equality Operators
+
+console.log([100] == 100); // true
+console.log([100] === 100); // false
+
+console.log('100' == 100); // true
+console.log('100' === 100); // false
+
+console.log([] == 0); // true
+console.log([] === 0); // false
+
+console.log('' == false); // true
+console.log('' === false); // false
+
+console.log('' == 0); // true
+console.log('' === 0); // false
+
+console.log(0 == false); // true
+console.log(0 === false); // false
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
