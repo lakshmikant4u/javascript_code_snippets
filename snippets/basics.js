cb3450d001d6e4f7566248c9a0cb038479a4957f
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-01-17 12:48:25 +0530

    Smooth scroll to a specific element

--- snippets/basics.js
+++ snippets/basics.js
@@ -364,3 +364,13 @@ const outer = () => {
 
 let innerFunc = outer();
 innerFunc(); // This is in outer func scope
+
+// 29. Smooth scroll to a specific element
+
+const element = document.getElementById("scrollId");
+
+element.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+    inline: "nearest",
+})
