08dce3b2ae6fe624bfcf59869e5e1226edabf699
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-02-18 00:03:17 +0530

    usage of from with three call back function of Observer

--- snippets/rxjs_in_details/rxjs_examples.js
+++ snippets/rxjs_in_details/rxjs_examples.js
@@ -8,3 +8,26 @@ const array = [1, 2, 'three', true];
 from(array)
     .subscribe((val) => console.log(val)); // 1,2, 'three' true
 
+// The simplest analogy is[].forEach(callback) :
+
+// array → Observable
+// forEach → subscribe
+// callback → Observer
+
+// Observer contains next, error, and complete functions… typescript way
+// export interface Observer<T> { 
+//     closed?: boolean;
+//     next: (value: T) => void;
+//     error: (err: any) => void;
+//     complete: () => void;
+// }
+
+// 1.1 from with all three call back function of Observer
+
+const array2 = [1, 2, 3];
+
+from(array2).subscribe({
+    next: (v) => console.log(v),
+    complete: console.info,
+    error: console.error
+});
